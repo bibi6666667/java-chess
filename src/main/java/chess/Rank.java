ff1412b7c876654fdb2a59ae56390612a2b9924a
Haina <bibi6666667@kakao.com>
2021-02-26 18:18:35 +0900

    [step5] Rank 클래스 추가
    체스판의 열을 담당하는 Rank클래스를 생성했습니다.

---
+++ src/main/java/chess/Rank.java
@@ -0,0 +1,11 @@
+package chess;
+
+import java.util.List;
+import java.util.ArrayList;
+import pieces.Piece;
+
+public class Rank {
+    private static int RANK_SIZE = 8;
+    public List<Piece> rank = new ArrayList<>(RANK_SIZE);
+
+}
